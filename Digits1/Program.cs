562bbff7f79f4edb1defd6062a1e838d1ae08684
Adrian Sims <adriansims62781@gmail.com>
2018-10-16 20:22:53 +0200

    Add project files.

---
+++ Digits1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digits1
+{
+    internal class Program
+    {
+        static int GetDigits(int x)
+        {
+            if (x > 9)
+            {
+                return GetDigits(x / 10);
+            }
+            Console.WriteLine($"{x}");
+            return 0;
+        }
+
+        private static void Main(string[] args)
+        {
+            try
+            {
+                GetDigits(12345);
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add Digits1/Program.cs
+git add Digits1/App.config
+git add Digits1/Properties/AssemblyInfo.cs
+git add Digits1/Digits1.csproj
+git add Digits1.sln
+git add .gitignore
